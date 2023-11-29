43239a1ce404c771326a07c143054bd6b33d53b9
pyusuket <101777200+pyusuket@users.noreply.github.com>
2023-11-29 14:35:54 +0000

    [Add] 2-1 ルーティングの記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'lists/index'
   get 'lists/show'
   get 'lists/edit'
-  get '/top'
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
