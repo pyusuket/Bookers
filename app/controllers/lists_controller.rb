232501978c41a64d01f8d00e2fbabced5d94c518
pyusuket <101777200+pyusuket@users.noreply.github.com>
2023-11-30 15:25:51 +0000

    [Add] 3-1,3-2,3-3,3-4 top,indexページのレイアウト作成とcreate機能の追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,15 @@
 class ListsController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
+  end
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def index
@@ -9,5 +19,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
