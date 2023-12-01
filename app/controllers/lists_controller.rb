5197c96a4d9d8bc676fbf7cdccae6f2c51a15d30
pyusuket <101777200+pyusuket@users.noreply.github.com>
2023-12-01 13:17:03 +0000

    [Add] 3-7 bookのeditページの作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,6 +20,7 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
   
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  get 'top' => 'home#top'
   post 'lists' => 'lists#create'
-  get 'lists'=> 'lists#index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
-  get 'lists/:id' => 'lists#show' ,as: 'list'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
